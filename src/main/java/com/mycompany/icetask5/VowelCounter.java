dcce21651a4f8610965bb8670911555b415748f5
RC_Student_lab <RC_Student_lab@10.0.0.21>
2024-10-04 19:24:13 +0000

    vowels

--- src/main/java/com/mycompany/icetask5/VowelCounter.java
+++ src/main/java/com/mycompany/icetask5/VowelCounter.java
@@ -27,5 +27,12 @@ public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a sentence: ");
         String input = scanner.nextLine();
+// Call the countVowels function and display the result
+        int numberOfVowels = countVowels(input);
+        System.out.println("Number of vowels: " + numberOfVowels);
+        
+        scanner.close();
+    }
+}
 
     
